d16a86efe18c165a90ae4c071dab0399de642852
Daksh bajaj <dbajaj8991@conestoga.on.ca>
2026-02-22 12:41:09 -0500

    changed test method names and input values

--- CSCN71020_A2/TestProject/TestProject.cpp
+++ CSCN71020_A2/TestProject/TestProject.cpp
@@ -10,20 +10,68 @@ namespace TestProject
 	{
 	public:
 
-		TEST_METHOD(TestGetPerimeter_ValidDimensions_ReturnsCorrectValue)
+		TEST_METHOD(TestGetPerimeter_Validinput)
 		{
 			int length = 92;
 			int width = 3;
-			int expectedPerimeter = 190;
+			int expectedPerimeter = 190; //2*(92+3)
 			int actualPerimeter = getPerimeter(&length, &width);
 			Assert::AreEqual(expectedPerimeter, actualPerimeter);
 		}
-		TEST_METHOD(TestGetArea_ValidDimensions_ReturnsCorrectValue) {
-			int length = 520;
+		TEST_METHOD(TestGetArea_Validinput) {
+			int length = 56;
 			int width = 23;
-			int expectedArea = 11960;
+			int expectedArea = 1288; //56*23
 			int actualArea = getArea(&length, &width);
 			Assert::AreEqual(expectedArea, actualArea);
 		}
+        TEST_METHOD(TestSetLength_ValidInput)
+        {
+            // Arrange: Usual case within 1-99 range
+            int length = 67;
+            int input = 93;
+            setLength(input, &length);
+            Assert::AreEqual(input, length);
+        }
+
+        TEST_METHOD(TestSetLength_BelowRangeInput)
+        {
+            int length = 34;
+            int input = -44;
+            setLength(input, &length);
+            Assert::AreEqual(input, length);
+        }
+
+        TEST_METHOD(TestSetLength_AboveRangeInput)
+        {
+            // Arrange: Special case outside the 1-99 range
+            int initialLength = 65;
+            int invalidInput = 100;
+            setLength(invalidInput, &initialLength);
+            Assert::AreEqual(100, initialLength);
+        }
+        TEST_METHOD(TestSetWidth_ValidInput)
+        {
+            int width = 53;
+            int input = 45;
+            setWidth(input, &width);
+            Assert::AreEqual(input, width);
+        }
+
+        TEST_METHOD(TestSetWidth_BelowRangeInput)
+        {
+            int width = 98;
+            int input = -99;
+            setWidth(input, &width);
+            Assert::AreEqual(input, width);
+        }
+
+        TEST_METHOD(TestSetWidth_AboveRangeInput)
+        {
+            int initialWidth = 10;
+            int invalidInput = 109;
+            setWidth(invalidInput, &initialWidth);
+            Assert::AreEqual(109, initialWidth);
+        }
 	};
 }
